8df749f3ebc57e892caa204245a35d505b47b5d0
nopothegamer <40329022+nopothegamer@users.noreply.github.com>
2021-04-13 21:25:03 +1000

    Update GuiProfileViewer.java

--- src/main/java/io/github/moulberry/notenoughupdates/profileviewer/GuiProfileViewer.java
+++ src/main/java/io/github/moulberry/notenoughupdates/profileviewer/GuiProfileViewer.java
@@ -2129,7 +2129,7 @@ public class GuiProfileViewer extends GuiScreen {
 
 
         float fairySouls = Utils.getElementAsFloat(Utils.getElement(profileInfo, "fairy_souls_collected"), 0);
-        Utils.renderAlignedString(EnumChatFormatting.LIGHT_PURPLE+"Fairy Souls", EnumChatFormatting.WHITE.toString()+(int)fairySouls+"/220",
+        Utils.renderAlignedString(EnumChatFormatting.LIGHT_PURPLE+"Fairy Souls", EnumChatFormatting.WHITE.toString()+(int)fairySouls+"/222",
                 guiLeft+xStart, guiTop+yStartBottom, 76);
         if(skillInfo != null) {
             float totalSkillLVL = 0;
