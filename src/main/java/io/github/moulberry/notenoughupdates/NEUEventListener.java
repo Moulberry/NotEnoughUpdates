0bc546523db38beb6db8b34e5c5b12fb7e56dc9c
nopothegamer <40329022+nopothegamer@users.noreply.github.com>
2021-10-19 11:30:58 +1100

    comment

--- src/main/java/io/github/moulberry/notenoughupdates/NEUEventListener.java
+++ src/main/java/io/github/moulberry/notenoughupdates/NEUEventListener.java
@@ -799,12 +799,7 @@ public class NEUEventListener {
 
                 chatComponent.setChatStyle(Utils.createClickStyle(ClickEvent.Action.RUN_COMMAND, "/pv " + username, "" + EnumChatFormatting.YELLOW + "Click to open " + EnumChatFormatting.AQUA + EnumChatFormatting.BOLD + username + EnumChatFormatting.RESET + EnumChatFormatting.YELLOW + "'s profile in " + EnumChatFormatting.DARK_PURPLE + EnumChatFormatting.BOLD + "NEU's" + EnumChatFormatting.RESET + EnumChatFormatting.YELLOW + " profile viewer."));
                 return chatComponent;
-            } else if (chatComponent.getChatStyle().getChatClickEvent().getValue().startsWith("/viewprofile")) {
-                String username = chatComponent.getChatStyle().getChatClickEvent().getValue().substring(13);
-
-                chatComponent.setChatStyle(Utils.createClickStyle(ClickEvent.Action.RUN_COMMAND, "/pv " + username, "" + EnumChatFormatting.YELLOW + "Click to open " + EnumChatFormatting.AQUA + EnumChatFormatting.BOLD + username + EnumChatFormatting.RESET + EnumChatFormatting.YELLOW + "'s profile in " + EnumChatFormatting.DARK_PURPLE + EnumChatFormatting.BOLD + "NEU's" + EnumChatFormatting.RESET + EnumChatFormatting.YELLOW + " profile viewer."));
-                return chatComponent;
-            }
+            } // wanted to add this for guild but guild uses uuid :sad:
         }
         return chatComponent;
     }
