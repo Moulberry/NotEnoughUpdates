f997510fd8d522756aa6fff2f905bfe4f5c3c8e3
NopoTheGamer <40329022+NopoTheGamer@users.noreply.github.com>
2022-11-09 13:24:33 +0000

    fix npe in the chat thingy (#431)

--- src/main/java/io/github/moulberry/notenoughupdates/listener/ChatListener.java
+++ src/main/java/io/github/moulberry/notenoughupdates/listener/ChatListener.java
@@ -127,7 +127,7 @@ public class ChatListener {
 			boolean partyOrGuildChat = false;
 
 			List<IChatComponent> siblings = chatComponent.getSiblings();
-			if (!siblings.isEmpty() && siblings.get(0).getChatStyle().getChatClickEvent().getValue().startsWith("/viewprofile")) {
+			if (!siblings.isEmpty() && siblings.get(0).getChatStyle() != null && siblings.get(0).getChatStyle().getChatClickEvent() != null && siblings.get(0).getChatStyle().getChatClickEvent().getValue().startsWith("/viewprofile")) {
 				startsWith = "/viewprofile";
 				partyOrGuildChat = true;
 			} else {
