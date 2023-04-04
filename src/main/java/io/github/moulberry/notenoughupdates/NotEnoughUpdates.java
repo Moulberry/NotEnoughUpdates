e00d51e54b9c7303097ecd264412ed7b98f1f804
NopoTheGamer <40329022+NopoTheGamer@users.noreply.github.com>
2023-04-05 04:07:11 +1000

    Reset the api url if its empty (#672)
    kids keep deleting the api url

--- src/main/java/io/github/moulberry/notenoughupdates/NotEnoughUpdates.java
+++ src/main/java/io/github/moulberry/notenoughupdates/NotEnoughUpdates.java
@@ -252,6 +252,10 @@ public class NotEnoughUpdates {
 				config.apiData.repoBranch = "prerelease";
 			}
 
+			if (config.apiData.moulberryCodesApi.isEmpty()) {
+				config.apiData.moulberryCodesApi = "moulberry.codes";
+			}
+
 			saveConfig();
 		}
 
