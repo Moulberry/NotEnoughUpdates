06bee6784eaf8748e3547f1fafeebe71010566ff
Ad_amko <83232017+Okmada@users.noreply.github.com>
2023-04-07 02:53:31 +0200

    Farming overlay: Nether warts swap from alchemy xp to farming xp (#679)
    Co-authored-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/io/github/moulberry/notenoughupdates/overlays/FarmingSkillOverlay.java
+++ src/main/java/io/github/moulberry/notenoughupdates/overlays/FarmingSkillOverlay.java
@@ -48,7 +48,6 @@ public class FarmingSkillOverlay extends TextOverlay {
 	private int cultivating = -1;
 	private int cultivatingTier = -1;
 	private String cultivatingTierAmount = "1";
-	private int alchemy = 0;
 	private int foraging = 0;
 	private double coins = -1;
 	private float cropsPerSecondLast = 0;
@@ -204,23 +203,18 @@ public class FarmingSkillOverlay extends TextOverlay {
 
 			//Set default skillType to Farming and get BZ price config value
 			skillType = "Farming";
-			alchemy = 0;
 			foraging = 0;
 
-			//WARTS
+			//WOOD
 			boolean useBZPrice = NotEnoughUpdates.INSTANCE.config.skillOverlays.useBZPrice;
-			if (internalName.startsWith("THEORETICAL_HOE_WARTS")) {
-				skillType = "Alchemy";
-				alchemy = 1;
-				foraging = 0;
-				coins = useBZPrice ? getCoinsBz("ENCHANTED_NETHER_STALK", ENCH_SIZE) : 2;
-
-				//WOOD
-			} else if (internalName.equals("TREECAPITATOR_AXE") || internalName.equalsIgnoreCase("JUNGLE_AXE")) {
+			if (internalName.equals("TREECAPITATOR_AXE") || internalName.equalsIgnoreCase("JUNGLE_AXE")) {
 				skillType = "Foraging";
-				alchemy = 0;
 				foraging = 1;
 				coins = 2;
+
+				//WARTS
+			} else  if (internalName.startsWith("THEORETICAL_HOE_WARTS")) {
+				coins = useBZPrice ? getCoinsBz("ENCHANTED_NETHER_STALK", ENCH_SIZE) : 2;
 
 				//COCOA
 			} else if (internalName.equals("COCO_CHOPPER")) {
@@ -549,28 +543,23 @@ public class FarmingSkillOverlay extends TextOverlay {
 
 			}
 
-			if (skillInfo != null && skillInfo.level == 60 || alchemy == 1 && skillInfo != null && skillInfo.level == 50) {
+			if (skillInfo != null && skillInfo.level == 60) {
 				int current = (int) skillInfo.currentXp;
 				if (skillInfoLast != null && skillInfo.currentXpMax == skillInfoLast.currentXpMax) {
 					current = (int) interp(current, skillInfoLast.currentXp);
 				}
 
-				if (alchemy == 0) {
+				if (foraging == 0) {
 					lineMap.put(
 						2,
 						EnumChatFormatting.AQUA + "Farming: " + EnumChatFormatting.YELLOW + "60 " + EnumChatFormatting.RED +
 							"(Maxed)"
 					);
-				} else if (foraging == 1) {
+				} else {
 					lineMap.put(
 						2,
 						EnumChatFormatting.AQUA + "Foraging: " + EnumChatFormatting.YELLOW + "50 " + EnumChatFormatting.RED +
 							"(Maxed)"
-					);
-				} else {
-					lineMap.put(
-						2,
-						EnumChatFormatting.AQUA + "Alch: " + EnumChatFormatting.YELLOW + "50 " + EnumChatFormatting.RED + "(Maxed)"
 					);
 				}
 				lineMap.put(3, EnumChatFormatting.AQUA + "Current XP: " + EnumChatFormatting.YELLOW + format.format(current));
