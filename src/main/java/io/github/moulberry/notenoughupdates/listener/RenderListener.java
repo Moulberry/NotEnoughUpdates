2d05e43d0cac6221f932da92e6fed4db11da244a
heyngra <heyngra.wspolpraca@gmail.com>
2023-06-27 10:32:38 +0200

    Change kismet in dungeon profits to bazaar (#737)

--- src/main/java/io/github/moulberry/notenoughupdates/listener/RenderListener.java
+++ src/main/java/io/github/moulberry/notenoughupdates/listener/RenderListener.java
@@ -777,7 +777,8 @@ public class RenderListener {
 							}
 						}
 					}
-					double kismetPrice = neu.manager.auctionManager.getLowestBin("KISMET_FEATHER");
+					JsonObject kismetBazaar = neu.manager.auctionManager.getBazaarInfo("KISMET_FEATHER");
+					double kismetPrice = (kismetBazaar != null && kismetBazaar.has("curr_buy")) ? kismetBazaar.get("curr_buy").getAsFloat() : 0;
 					String kismetStr = EnumChatFormatting.RED + formatCoins(kismetPrice) + " coins";
 					if (neu.config.dungeons.useKismetOnDungeonProfit)
 						profitLossBIN = kismetUsed ? profitLossBIN - kismetPrice : profitLossBIN;
